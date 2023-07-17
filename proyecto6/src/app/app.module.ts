ea8aa3826f37735a10b37fe6d0d1acf7e40c1489
Meiyin Chang <91926069+meiyincr3@users.noreply.github.com>
2023-07-17 11:28:22 -0500

    Update app.module.ts

--- proyecto6/src/app/app.module.ts
+++ proyecto6/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from  '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -11,7 +11,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { CalltoactionComponent } from './calltoaction/calltoaction.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from  '@angular/common/http';
+
 
 @NgModule({
   declarations: [
