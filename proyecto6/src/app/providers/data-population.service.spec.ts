2b18ad2db013836506cd7af13868e83f349e6ab8
Meiyin Chang <mmchang@espol.edu.ec>
2023-07-19 14:49:06 -0500

    pendiente completar test

--- proyecto6/src/app/providers/data-population.service.spec.ts
+++ proyecto6/src/app/providers/data-population.service.spec.ts
@@ -1,13 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { dataPopulationService } from './data-population.service';
+
+  //Importe la interfaz
+  import { Population} from '../interfaces/population';
 
 describe('dataPopulationService', () => {
   let service: dataPopulationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      
+        //Registre el módulo de petición http
+        imports: [ HttpClientModule ],
+
+        //Registre el servicio como proveedor de datos
+        providers: [ dataPopulationService ]
+    });
     service = TestBed.inject(dataPopulationService);
+  });
+  // La función done se encarga de esperar por completar el requerimiento
+  it(' ngOnInit should return value from observable', (done: DoneFn) => {
+    //Invoque el método con la petición asincrónica
+    service.getResponse().subscribe(data => {
+      
+      // Valide que la respuesta sea mayor que 0
+      expect((data as Population[]).length).toBeGreaterThan(0)
+      
+      // Que espere hasta que llegue la respuesta 
+      done();
+    });
   });
 
   it('should be created', () => {
--- proyecto6/src/app/services/services.component.spec.ts
+++ proyecto6/src/app/services/services.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { ServicesComponent } from './services.component';
+import { dataPopulationService } from '../providers/data-population.service';
 
 describe('ServicesComponent', () => {
   let component: ServicesComponent;
@@ -8,14 +9,33 @@ describe('ServicesComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ServicesComponent]
+      //Registre el módulo de petición http
+      imports: [HttpClientModule],
+
+      //Registre el servicio como proveedor de datos
+      providers: [dataPopulationService],
+      declarations: [ServicesComponent],
     });
     fixture = TestBed.createComponent(ServicesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should request service user after Angular calls ngOnInit', (done: DoneFn) => {
+    // Llame a ngOnInit para simular el ciclo de vida del componente
+    component.ngOnInit();
+
+    // Utilice fixture.whenStable para esperar a que se resuelva el observable del servicio
+    fixture.whenStable().then(() => {
+      // Valide que la respuesta sea mayor que 0
+      expect(component.data.length).toBeGreaterThan(0);
+
+      // Que espere hasta que llegue la respuesta
+      done();
+
+      it('should create', () => {
+        expect(component).toBeTruthy();
+      });
+    });
   });
 });
