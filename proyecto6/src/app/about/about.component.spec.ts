4afd2a0e685bc9a4693495758ef9e84f65e07201
Meiyin Chang <mmchang@espol.edu.ec>
2023-07-19 14:49:16 -0500

    completar

--- proyecto6/src/app/about/about.component.spec.ts
+++ proyecto6/src/app/about/about.component.spec.ts
@@ -6,9 +6,25 @@ describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
+  //Valide la existencia de un elemento <nav> en la plantilla html del elemento
+
+  it('nav element', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const nav = headerElement.querySelector('nav')!;
+    expect(nav).toBeTruthy();
+  });
+
+  //Valide la NO existencia de un elemento <p> en la plantilla html del elemento
+
+  it('No p element', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const nav = headerElement.querySelector('p')!;
+    expect(nav).toBeFalsy();
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AboutComponent]
+      declarations: [AboutComponent],
     });
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
